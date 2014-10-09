98e99941bf4a4a27e61229e8be16a2c34e608103
Contra <contra@maricopa.edu>
2014-10-09 08:58:02 -0500

    formatting

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var gql = require('gql');
 module.exports = function(){
   var query = gql.query();
   query.needs(1);
-  query.exact("rs601338", "AA");
+  query.exact('rs601338', 'AA');
   return query;
 };
